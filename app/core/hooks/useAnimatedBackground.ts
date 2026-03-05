78ea1abe5b143c2dffb0259e311c04468c20058e
Ahmed-Emad-Nasr <ahmedemadeldeen77@gmail.com>
2026-03-05 17:40:24 +0200

    .

--- app/core/hooks/useAnimatedBackground.ts
+++ app/core/hooks/useAnimatedBackground.ts
@@ -69,7 +69,7 @@ export const useAnimatedBackground = (canvasRef: React.RefObject<HTMLCanvasEleme
   }, [dimensions, isMobile]);
 
   const createMeteors = useCallback(() => {
-    const reductionFactor = isMobile ? 0.62 : 0.72;
+    const reductionFactor = isMobile ? 0.7 : 0.7;
     const numberOfMeteors = Math.floor((dimensions.width / 250) * reductionFactor);
     const meteors = Array.from({ length: numberOfMeteors }, () => ({
       x: Math.floor(Math.random() * (dimensions.width / gridSize)) * gridSize,
