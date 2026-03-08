292d63419b2f10717ee2169a753bd1d3949bf609
Ahmed-Emad-Nasr <ahmedemadeldeen77@gmail.com>
2026-03-08 20:44:11 +0200

    reduced number of meteors on mobile devices to improve performance. The number of meteors is now calculated based on the screen width, with a maximum of 0.9 meteors per 250 pixels of width.

--- app/core/hooks/useAnimatedBackground.ts
+++ app/core/hooks/useAnimatedBackground.ts
@@ -126,7 +126,7 @@ export const useAnimatedBackground = (canvasRef: React.RefObject<HTMLCanvasEleme
   }, [dimensions, isMobile]);
 
   const createMeteors = useCallback(() => {
-    const numberOfMeteors = Math.floor(dimensions.width / 250);
+    const numberOfMeteors = Math.floor((dimensions.width / 250) * 0.9);
     meteorsRef.current = Array.from({ length: numberOfMeteors }, () => ({
       x: Math.floor(Math.random() * (dimensions.width / gridSize)) * gridSize,
       y: Math.floor(Math.random() * (dimensions.height / gridSize)) * gridSize,
