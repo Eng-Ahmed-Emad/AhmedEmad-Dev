f343e35bd60749d0878a644396d01338a59824c3
Ahmed-Emad-Nasr <ahmedemadeldeen77@gmail.com>
2026-03-08 03:22:34 +0200

    reduce image quality to 50 for better performance while maintaining decent visual quality.

--- app/components/art_gallery/sensei-art.tsx
+++ app/components/art_gallery/sensei-art.tsx
@@ -70,7 +70,7 @@ const ImageItem = React.memo(({ image, index, setOpen }: ImageItemProps) => {
                 onClick={() => setOpen(index)}
                 loading="lazy"
                 decoding="async"
-                quality={55}
+                quality={50}
                 className={styles.galleryImg}
             />
         </motion.div>
